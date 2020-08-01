778bdfe6010b69df3206a02df80da84c672c0b8e
Ehsan Fazel <44735466+efazel@users.noreply.github.com>
2020-08-01 16:40:12 -0300

    Update Ang_et_al_sorting.m

--- Ang_et_al_sorting.m
+++ Ang_et_al_sorting.m
@@ -1,4 +1,4 @@
-%% This document performs the same estimating process as Ang-et-al-2006.
+%% This document performs the same estimating process as Ang-et-al-2006 %%
 % To reproduce table 1 with concentration and sparsity  we can do the following:
 % Either regress the excess returns on market factor and NC or NS
 % (separately or together)
@@ -7,7 +7,7 @@
 clear;
 clc;
 
-%% For now just regress on NS and NC and Mrk and sort: 
+%% For now just regress on NS and NC and Mrk and sort %%
 
 %monthly return for common stock from 1976-2018
 load('raw_CS_return.mat'); 
@@ -31,7 +31,7 @@ load('VIXmonthly.mat')
 
 CONCENTRATION = VIXmonthly;
 
-%% for implied volatility, lets only look at 1996 to 2013 total of 216 months
+%% for implied volatility, lets only look at 1996 to 2013 total of 216 months %%
 t1 = datetime(1975,12,1);
 t = t1 + calmonths(1:516);
 t = t';
@@ -45,7 +45,7 @@ cs_rtn = raw_CS_return(242:458,:);
 %common stock market cap to Jan 2014-one month ahead
 cs_cap = raw_CS_cap(242:458,:); 
 
-%% Test for NC in innovation
+%% Test for NC in innovation %%
 inno_NC = diff(CONCENTRATION);
 
 %common stock return
@@ -77,14 +77,14 @@ CONCENTRATION = inno_NC;
 % end
 
 
-%% Rolling window analysis: at each period t we construct portfolios by 
+%% Rolling window analysis: at each period t we construct portfolios by %%
 % regressing values form t-1 to t-w - same as Herskovic
 
-%window length
+% window length
 w = 24; 
 k = size(cs_rtn,1);
 
-%number of intervals/number of portfolios
+% number of intervals/number of portfolios
 N = k - w ; 
 
 for i=1:N
@@ -107,7 +107,7 @@ P_t(:,NaN_rmv) = [];
 R_t(:,NaN_rmv) = [];
 
 % We should look at the same firms in P_t and find their returns fron t-1 to
-%t-w by searching P_t in R and consider those that have at least 21 points 
+% t-w by searching P_t in R and consider those that have at least 21 points 
 size_P_t = size(P_t,2); %how many firms to look for
 P_t_1 = P_t;
 r_t_1 = zeros(w,size_P_t); %return series from t-w to t-1
@@ -121,12 +121,12 @@ for j=1:size_P_t
     c_t_1(:,j) = c;
 end
 
-%check for NaN in r_t_1
+% check for NaN in r_t_1
 num_NaN_t_1 = isnan(r_t_1); 
 total_num_NaN_t_1 = sum(num_NaN_t_1);
 NaN_rmv = find(total_num_NaN_t_1 >= 3);
 
-%remove them
+% remove them
 r_t_1(:,NaN_rmv) = []; 
 P_t_1(:,NaN_rmv) = [];
 r_t_1_noNaN = fillmissing(r_t_1,'linear');
@@ -155,7 +155,7 @@ end
 cs_rtn = raw_CS_return(242:457,:);
 cs_cap = raw_CS_cap(242:457,:); %common stock market cap
 
-%% creating window frames for RF and Market factor and Concentration
+%% creating window frames for RF and Market factor and Concentration %%
 rf = RF_96_2013;
 mrkt = mrkt_96_2013;
 
@@ -172,7 +172,7 @@ rf(1,:) = [];
 CONCENTRATION(1,:) = [];
 end
 
-%% Running regressions for windows
+%% Running regressions for windows %%
 % we need R_t_1 & R_t_cell , MARKET and RF and a column of one each time
 % and also the factors. For now: concentration with 2 versions
 % dont forget to adjust the dimension since you would regress on both level
@@ -228,10 +228,10 @@ qnt = 5;
 for i=1:N
 
 
-%total sorted firms for the window
+% total sorted firms for the window
 totalsorted = sortedNC{i}; 
 
-%dimension of each quitile (num of firms in each)
+% dimension of each quitile (num of firms in each)
 q1_num = round(size(totalsorted,2)/qnt);
 q5_num = q1_num; 
 q2_num = q1_num;
@@ -242,7 +242,7 @@ QUINTILES{i} = mat2cell(totalsorted,3,quintile_sizes); %in each cell there is on
 
 end
 
-%% Creating portfolios in period (t) from QUINTILES
+%% Creating portfolios in period (t) from QUINTILES %%
 % we look at permnos in each QUINTILES and return their corresponding return
 % and market cap in period t in R_t_cell. so we can creat the value-weighted
 % returns.
@@ -270,7 +270,7 @@ for i=1:N
     
 end
 
-%calculate value weighted returns for each portfolio
+% calculate value weighted returns for each portfolio
 prtfl_returns = zeros(N,qnt);
 for i=1:N
     for j=1:qnt
@@ -282,5 +282,5 @@ for i=1:N
     end
 end
 
-%% test
+%% test %%
 [h,p,ci,stats]=ttest2(prtfl_returns(:,1),prtfl_returns(:,5))
