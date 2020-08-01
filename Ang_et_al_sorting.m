9873609b6ebd742e5e4316357b1bdb2ea32e567a
Ehsan Fazel <44735466+efazel@users.noreply.github.com>
2020-08-01 16:34:05 -0300

    Update Ang_et_al_sorting.m

--- Ang_et_al_sorting.m
+++ Ang_et_al_sorting.m
@@ -1,37 +1,58 @@
-%% this code performs the same estimting process as Ang et al. 2006
-% To replicate the first table we can do the following:
-% either regress the excess returns on market factor and NC or NS
+%% This document performs the same estimating process as Ang-et-al-2006
+% To reproduce table 1 with concentration and sparsity  we can do the following:
+% Either regress the excess returns on market factor and NC or NS
 % (separately or together)
-%or just regress on NC and NS 
+% or just regress on NC and NS 
+
 clear;
 clc;
-%for now just regress on NS and NC and Mrk and sort single way 
 
-load('raw_CS_return.mat'); %monthly return for common stock from 1976-2018
-load('raw_CS_cap.mat');%
+%% For now just regress on NS and NC and Mrk and sort: 
 
-load('mrkt_96_2013.mat'); %market factor
-load('RF_96_2013.mat'); %risk free rate
+%monthly return for common stock from 1976-2018
+load('raw_CS_return.mat'); 
 
-load('Concentration2.mat'); %loading factors
+% monthly cap for common stock from 1976-2018
+load('raw_CS_cap.mat'); 
+
+%market factor
+load('mrkt_96_2013.mat');  
+
+%risk free rate
+load('RF_96_2013.mat'); 
+
+%loading factors (concentration and sparsity)
+load('Concentration2.mat'); 
 load('Concentration1.mat');
 
-load('VIXmonthly.mat') %load VIX
+%load VIX
+load('VIXmonthly.mat') 
 %inno_VIX = diff(VIX);
 
 CONCENTRATION = VIXmonthly;
+
 %% for implied volatility, lets only look at 1996 to 2013 total of 216 months
 t1 = datetime(1975,12,1);
 t = t1 + calmonths(1:516);
 t = t';
 
-permno = raw_CS_return(1,:); %permno header for each col
-cs_rtn = raw_CS_return(242:458,:); %common stock return to Jan 2014-one month ahead
-cs_cap = raw_CS_cap(242:458,:); %common stock market cap to Jan 2014-one month ahead
-%% test for NC in innovation
+% PERMNO header for each col
+permno = raw_CS_return(1,:); 
+
+%common stock return to Jan 2014-one month ahead
+cs_rtn = raw_CS_return(242:458,:); 
+
+%common stock market cap to Jan 2014-one month ahead
+cs_cap = raw_CS_cap(242:458,:); 
+
+%% Test for NC in innovation
 inno_NC = diff(CONCENTRATION);
-cs_rtn = raw_CS_return(243:457,:); %common stock return
-cs_cap = raw_CS_cap(243:457,:); %common stock market cap
+
+%common stock return
+cs_rtn = raw_CS_return(243:457,:); 
+
+%common stock market cap
+cs_cap = raw_CS_cap(243:457,:); 
 mrkt_96_2013 = mrkt_96_2013(2:end,:);
 RF_96_2013 = RF_96_2013(2:end,:);
 CONCENTRATION = inno_NC;
@@ -54,12 +75,17 @@ CONCENTRATION = inno_NC;
 %     P_R_CELL{i} = P_R; %the cell that contain biennial data
 %     R(1:24,:) = [];% remove the first two years in each loop
 % end
-%% Rolling window analysis: at each period t we construct portfolios by 
-%regressing values form t-1 to t-w - same as Herskovic
 
-w = 24; %window length
+
+%% Rolling window analysis: at each period t we construct portfolios by 
+% regressing values form t-1 to t-w - same as Herskovic
+
+%window length
+w = 24; 
 k = size(cs_rtn,1);
-N = k - w ; %number of intervals/number of portfolios
+
+%number of intervals/number of portfolios
+N = k - w ; 
 
 for i=1:N
 P = permno; %global set of permno
@@ -80,7 +106,7 @@ C_t(:,NaN_rmv) = [];
 P_t(:,NaN_rmv) = [];
 R_t(:,NaN_rmv) = [];
 
-%we should look at the same firms in P_t and find their returns fron t-1 to
+% We should look at the same firms in P_t and find their returns fron t-1 to
 %t-w by searching P_t in R and consider those that have at least 21 points 
 size_P_t = size(P_t,2); %how many firms to look for
 P_t_1 = P_t;
@@ -94,15 +120,21 @@ for j=1:size_P_t
     r_t_1(:,j) = r; 
     c_t_1(:,j) = c;
 end
-num_NaN_t_1 = isnan(r_t_1); %check for NaN in r_t_1
+
+%check for NaN in r_t_1
+num_NaN_t_1 = isnan(r_t_1); 
 total_num_NaN_t_1 = sum(num_NaN_t_1);
 NaN_rmv = find(total_num_NaN_t_1 >= 3);
-r_t_1(:,NaN_rmv) = []; %remove them
+
+%remove them
+r_t_1(:,NaN_rmv) = []; 
 P_t_1(:,NaN_rmv) = [];
 r_t_1_noNaN = fillmissing(r_t_1,'linear');
 R_t_1{i} = [P_t_1;r_t_1_noNaN];
-%we need to go back to R_t and P_t and remove R(t) and their permno that
-%were not available in the window frame
+
+
+% we need to go back to R_t and P_t and remove R(t) and their permno that
+% were not available in the window frame
 r_t = zeros(1,size(r_t_1,2));
 cap = zeros(1,size(r_t_1,2));
 for k=1:size(r_t_1,2)
@@ -122,6 +154,7 @@ cs_cap(1,:) = [];
 end
 cs_rtn = raw_CS_return(242:457,:);
 cs_cap = raw_CS_cap(242:457,:); %common stock market cap
+
 %% creating window frames for RF and Market factor and Concentration
 rf = RF_96_2013;
 mrkt = mrkt_96_2013;
@@ -138,18 +171,19 @@ mrkt(1,:) = [];
 rf(1,:) = [];
 CONCENTRATION(1,:) = [];
 end
-%% Running regressions for windows. 
-% we need
-%R_t_1 & R_t_cell , MARKET and RF and a column of one each time
-% and also the factors: for now: concentration with 2 version
-%dont forget to adjust the dimension since you would regress on both level
-%and innovation
+
+%% Running regressions for windows
+% we need R_t_1 & R_t_cell , MARKET and RF and a column of one each time
+% and also the factors. For now: concentration with 2 versions
+% dont forget to adjust the dimension since you would regress on both level
+% and innovation
 
 %beta1 = zeros(1,N);
 %beta2 = zeros(1,N);
 %beta1_CI = cell(1,N);
 %beta2_CI = cell(1,N); No CI for now, I can add them later
-%declaring the independent variables
+
+% declaring the independent variables
 for i=1:N
     depends = R_t_1{i}(2:end,:); %set of y
     permno = R_t_1{i}(1,:);
@@ -187,14 +221,18 @@ for i=1:N
     sortedNC{i} = [permn;IndexNC;sorted_betanc];  %%yYOU HAVE WRITE THE CORRECT PERMNO
 end
 
-%in sortedNC, first row corresponding permno, 2nd row index of the firm,
-%3rd its sorted return
+% In sortedNC, first row corresponds to permno, 2nd row is index of the firm,
+% 3rd is the sorted return
 % creating quintile portfolios for each period N=192
 qnt = 5;
 for i=1:N
-    
-totalsorted = sortedNC{i}; %total sorted firms for the window
-q1_num = round(size(totalsorted,2)/qnt);%dimension of each quitile (num of firms in each)
+
+
+%total sorted firms for the window
+totalsorted = sortedNC{i}; 
+
+%dimension of each quitile (num of firms in each)
+q1_num = round(size(totalsorted,2)/qnt);
 q5_num = q1_num; 
 q2_num = q1_num;
 q4_num = q1_num;
@@ -204,10 +242,11 @@ QUINTILES{i} = mat2cell(totalsorted,3,quintile_sizes); %in each cell there is on
 
 end
 
-%% creating portfolios in period (t) from QUINTILES
-%we look at permnos in each QUINTILES and return their corresponding return
-%and market cap in period t in R_t_cell. so we can creat the value eighted
-%returns.
+%% Creating portfolios in period (t) from QUINTILES
+% we look at permnos in each QUINTILES and return their corresponding return
+% and market cap in period t in R_t_cell. so we can creat the value-weighted
+% returns.
+
 PRTFL = cell(N,qnt); %total portfolios
 for i=1:N
     all_quantiles = QUINTILES{1,i};
@@ -243,5 +282,5 @@ for i=1:N
     end
 end
 
-
+%% test
 [h,p,ci,stats]=ttest2(prtfl_returns(:,1),prtfl_returns(:,5))
